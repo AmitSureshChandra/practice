787d3a8248f5c1d6f31bcedc29bcd7b84abf40a2
Amit Sureshchandra Kesarwani <47358181+AmitSureshChandra@users.noreply.github.com>
2022-12-28 20:26:40 +0530

    Create INSTAGRAM.java

---
+++ src/main/java/com/dsa/practice/codeshef/starter-71/INSTAGRAM.java
@@ -0,0 +1,24 @@
+// https://www.codechef.com/START71D/problems/INSTAGRAM
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int following = sc.nextInt();
+		    int follower = sc.nextInt();
+		    
+		    if(following > 10 * follower) System.out.println("YES");
+		    else System.out.println("NO");
+		}
+	}
+}
